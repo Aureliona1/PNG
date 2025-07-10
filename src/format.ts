054ac975ccb03e05815e0a6c23b5ac518163f0af
Aureliona <62498557+Aureliona1@users.noreply.github.com>
2025-07-10 15:48:51 +0930

    Do formatting externally

---
+++ src/format.ts
@@ -0,0 +1,213 @@
+import type { PNG } from "./png.ts";
+import { colorFormatChannels, type DecodeResult } from "./types.ts";
+
+export class PNGFormatterTo {
+	// Helper functions to convert a 24-bit color to a number
+	private static c2n = (color: Uint8Array) => (color[0] << 16) + (color[1] << 8) + color[2];
+	private static n2c = (n: number) => new Uint8Array([(n & (0xff << 16)) >> 16, (n & (0xff << 8)) >> 8, n & 0xff]);
+	private _indexedPalette: Map<number, number> = new Map();
+	/**
+	 * A utility class that converts pixel arrays from RGBA to any supported format.
+	 * All methods assume that the image is already in valid RGBA, so this should be checked externally with {@link isRGBA}.
+	 * @param src The source image to format.
+	 */
+	constructor(public src: PNG) {}
+	/**
+	 * Check if the image is grayscale with no alpha.
+	 */
+	canBeGrayScale(): boolean {
+		let valid = true;
+		for (let i = 0; i < this.src.raw.length && valid; i += 4) {
+			valid = this.src.raw[i] === this.src.raw[i + 1] && this.src.raw[i] === this.src.raw[i + 2] && this.src.raw[i + 3] === 255;
+		}
+		return valid;
+	}
+	/**
+	 * Transform the pixel array into a single-channel grayscale array.
+	 * Does not mutate the old raw.
+	 * This does not check whether the image is eligible to be grayscale, this must be done externally.
+	 */
+	toGrayScale(): Uint8Array {
+		const newRaw = new Uint8Array(this.src.raw.length / 4);
+		for (let i = 0; i < newRaw.length; i++) {
+			newRaw[i] = this.src.raw[Math.floor(i / 4)];
+		}
+		return newRaw;
+	}
+	/**
+	 * Check if the image has no transparency.
+	 */
+	canBeRGB(): boolean {
+		let valid = true;
+		for (let i = 3; i < this.src.raw.length && valid; i += 4) {
+			valid = this.src.raw[i] === 255;
+		}
+		return valid;
+	}
+	/**
+	 * Transform the pixel array into RGB with no alpha.
+	 * Does not mutate the old raw.
+	 * This does not check if the image is eligible to be RGB, this must be done externally.
+	 */
+	toRGB(): Uint8Array {
+		const newRaw = new Uint8Array((this.src.raw.length * 3) / 4);
+		for (let i = 0; i < newRaw.length / 3; i++) {
+			newRaw[i * 3] = this.src.raw[i * 4];
+			newRaw[i * 3 + 1] = this.src.raw[i * 4 + 1];
+			newRaw[i * 3 + 2] = this.src.raw[i * 4 + 2];
+		}
+		return newRaw;
+	}
+	/**
+	 * Check if the image can be represented by indexed color. This is rather slow as it has to manually perform color indexing.
+	 */
+	canBeIndexed(): boolean {
+		// No point trying if we have alpha
+		if (!this.canBeRGB()) return false;
+
+		// Unique set of image colors.
+		this._indexedPalette.clear();
+		for (let i = 0; i < this.src.raw.length; i += 4) {
+			const key = PNGFormatterTo.c2n(this.src.raw.subarray(i, i + 3));
+			if (!this._indexedPalette.has(key)) {
+				this._indexedPalette.set(key, this._indexedPalette.size);
+			}
+		}
+
+		// If there are too many colors
+		if (this._indexedPalette.size > 256) {
+			this._indexedPalette.clear();
+			return false;
+		}
+
+		return true;
+	}
+	/**
+	 * Transform the pixel array into indexed color.
+	 * This does not mutate the old pixel array.
+	 * This will check that the image is eligible to be indexed, and will return two empty Uint8Arrays if the image is not.
+	 */
+	toIndexed(): [Uint8Array, Uint8Array] {
+		if (!this._indexedPalette.size) {
+			if (!this.canBeIndexed()) {
+				return [new Uint8Array(), new Uint8Array()];
+			}
+		}
+		const palette = new Uint8Array(this._indexedPalette.size * 3);
+		this._indexedPalette.forEach((i, col) => {
+			palette.set(PNGFormatterTo.n2c(col), i * 3);
+		});
+		const newRaw = new Uint8Array(this.src.raw.length / 4);
+		for (let i = 0; i < newRaw.length; i++) {
+			newRaw[i] = this._indexedPalette.get(PNGFormatterTo.c2n(this.src.raw.subarray(i * 4, i * 4 + 3))) ?? 0;
+		}
+		return [palette, newRaw];
+	}
+	/**
+	 * Check if the image can be represented as grayscale with alpha.
+	 */
+	canBeGrayScaleAlpha(): boolean {
+		if (this.canBeGrayScale()) {
+			let consistentAlpha = true;
+			for (let i = 3; i < this.src.raw.length && consistentAlpha; i += 4) {
+				consistentAlpha = this.src.raw[i] === 255;
+			}
+			return !consistentAlpha;
+		}
+		return false;
+	}
+	/**
+	 * Transform the pixel array into grayscale with alpha.
+	 * This does not mutate the old pixel array.
+	 */
+	toGrayScaleAlpha(): Uint8Array {
+		const newRaw = new Uint8Array(this.src.raw.length / 2);
+		for (let i = 0; i < newRaw.length; i += 2) {
+			newRaw[i] = this.src.raw[i * 4];
+			newRaw[i + 1] = this.src.raw[i * 4 + 3];
+		}
+		return newRaw;
+	}
+	/**
+	 * Check if an image is valid RGBA, this should be called before other validators.
+	 * @param png The png to validate, this method requires width and height.
+	 */
+	canBeRGBA(): boolean {
+		return this.src.width * this.src.height * 4 === this.src.raw.length;
+	}
+	/**
+	 * This does nothing, it is just to be complete with the color formats.
+	 */
+	toRGBA(): Uint8Array {
+		return this.src.raw;
+	}
+}
+
+export class PNGFormatterFrom {
+	/**
+	 * A utility class that converts pixel arrays from any supported format to RGBA.
+	 * All methods assume that the image is in the specified format, this shuold be checked externally with {@link isCorrectFormat()}.
+	 * @param src The source image to format.
+	 */
+	constructor(public src: DecodeResult) {}
+	/**
+	 * Validate that the input pixel array length matches the expected length for this particular color format.
+	 * Also validates color indices for indexed color.
+	 */
+	isCorrectFormat() {
+		if (this.src.colorFormat === "Indexed") {
+			let maxIndex = 0;
+			for (let i = 0; i < this.src.raw.length; i++) {
+				maxIndex = this.src.raw[i] > maxIndex ? this.src.raw[i] : maxIndex;
+			}
+			return this.src.width * this.src.height === this.src.raw.length && this.src.palette.length > maxIndex;
+		}
+		return this.src.width * this.src.height * colorFormatChannels.get(this.src.colorFormat) === this.src.raw.length;
+	}
+	/**
+	 * Tranform a grayscale pixel array to RGBA.
+	 * Does not mutate the original array.
+	 */
+	fromGrayScale(): Uint8Array {
+		const newRaw = new Uint8Array(this.src.raw.length * 4);
+		for (let i = 0; i < newRaw.length; i++) {
+			newRaw[i] = (i + 1) % 4 ? this.src.raw[Math.floor(i / 4)] : 255;
+		}
+		return newRaw;
+	}
+	/**
+	 * Tranform an RGB pixel array to RGBA.
+	 * Does not mutate the original array.
+	 */
+	fromRGB(): Uint8Array {
+		const newRaw = new Uint8Array((this.src.raw.length * 4) / 3);
+		for (let i = 0, oldI = 0; i < newRaw.length; i++) {
+			newRaw[i] = (i + 1) % 4 ? this.src.raw[oldI++] : 255;
+		}
+		return newRaw;
+	}
+	/**
+	 * Tranform an indexed pixel array to RGBA.
+	 * Does not mutate the original array.
+	 */
+	fromIndexed(): Uint8Array {
+		const newRaw = new Uint8Array(this.src.raw.length * 4);
+		if (this.src.colorFormat == "Indexed") {
+			for (let i = 0; i < newRaw.length; i++) {
+				newRaw[i] = (i + 1) % 4 ? this.src.palette[this.src.raw[Math.floor(i / 4)] * 3 + (i % 4)] : 255;
+			}
+		}
+		return newRaw;
+	}
+	/**
+	 * Tranform a grayscale pixel array with alpha to RGBA.
+	 * Does not mutate the original array.
+	 */
+	fromGrayScaleAlpha(): Uint8Array {
+		const newRaw = new Uint8Array(this.src.raw.length * 2);
+		for (let i = 0; i < newRaw.length; i++) {
+			newRaw[i] = i % 4 === 2 ? this.src.raw[Math.floor(i / 2) - 1] : this.src.raw[Math.floor(i / 2)];
+		}
+		return newRaw;
+	}
+}
--- src/png.ts
+++ src/png.ts
@@ -5,139 +5,7 @@ import { encode } from "./binary/encode.ts";
 import { PNGDraw } from "./draw.ts";
 import { PNGFilter } from "./filters.ts";
 import type { BitDepth, ColorFormat } from "./types.ts";
-
-class PNGFormatter {
-	// Helper functions to convert a 24-bit color to a number
-	private static c2n = (color: Uint8Array) => (color[0] << 16) + (color[1] << 8) + color[2];
-	private static n2c = (n: number) => new Uint8Array([(n & (0xff << 16)) >> 16, (n & (0xff << 8)) >> 8, n & 0xff]);
-	private _indexedPalette: Map<number, number> = new Map();
-	/**
-	 * A utility class for validating and converting the image into supported PNG formats.
-	 * @param src The source image to format.
-	 */
-	constructor(public src: PNG) {}
-	/**
-	 * Check if the image is gray scale with no alpha.
-	 */
-	isGrayScale(): boolean {
-		let valid = true;
-		for (let i = 0; i < this.src.raw.length && valid; i += 4) {
-			valid = this.src.raw[i] === this.src.raw[i + 1] && this.src.raw[i] === this.src.raw[i + 2] && this.src.raw[i + 3] === 255;
-		}
-		return valid;
-	}
-	/**
-	 * Transform the pixel array into a single-channel grayscale array.
-	 * Does not mutate the old raw.
-	 * This does not check whether the image is eligible to be grayscale, this must be done externally.
-	 */
-	toGrayScale(): Uint8Array {
-		const newRaw = new Uint8Array(this.src.raw.length / 4);
-		for (let i = 0; i < newRaw.length; i++) {
-			newRaw[i] = this.src.raw[Math.floor(i / 4)];
-		}
-		return newRaw;
-	}
-	/**
-	 * Check if the image has no transparency.
-	 */
-	isRGB(): boolean {
-		let valid = true;
-		for (let i = 3; i < this.src.raw.length && valid; i += 4) {
-			valid = this.src.raw[i] === 255;
-		}
-		return valid;
-	}
-	/**
-	 * Transform the pixel array into RGB with no alpha.
-	 * Does not mutate the old raw.
-	 * This does not check if the image is eligible to be RGB, this must be done externally.
-	 */
-	toRGB(): Uint8Array {
-		const newRaw = new Uint8Array((this.src.raw.length * 3) / 4);
-		for (let i = 0; i < newRaw.length / 3; i++) {
-			newRaw[i * 3] = this.src.raw[i * 4];
-			newRaw[i * 3 + 1] = this.src.raw[i * 4 + 1];
-			newRaw[i * 3 + 2] = this.src.raw[i * 4 + 2];
-		}
-		return newRaw;
-	}
-	/**
-	 * Check if the image can be represented by indexed color. This is rather slow as it has to manually perform color indexing.
-	 */
-	isIndexed(): boolean {
-		// Gotta comment ts bc there's a lot going on here
-
-		// No point trying if we have alpha
-		if (!this.isRGB()) return false;
-
-		// Unique set of image colors.
-		this._indexedPalette.clear();
-		for (let i = 0; i < this.src.raw.length; i += 4) {
-			const key = PNGFormatter.c2n(this.src.raw.subarray(i, i + 3));
-			if (!this._indexedPalette.has(key)) {
-				this._indexedPalette.set(key, this._indexedPalette.size);
-			}
-		}
-
-		// If there are too many colors
-		if (this._indexedPalette.size > 256) {
-			this._indexedPalette.clear();
-			return false;
-		}
-
-		return true;
-	}
-	/**
-	 * Transform the pixel array into indexed color.
-	 * This does not mutate the old pixel array.
-	 * This will check that the image is eligible to be indexed, and will return two empty Uint8Arrays if the image is not.
-	 */
-	toIndexed(): [Uint8Array, Uint8Array] {
-		if (!this._indexedPalette.size) {
-			if (!this.isIndexed()) {
-				return [new Uint8Array(), new Uint8Array()];
-			}
-		}
-		const palette = new Uint8Array(this._indexedPalette.size * 3);
-		this._indexedPalette.forEach((i, col) => {
-			palette.set(PNGFormatter.n2c(col), i * 3);
-		});
-		const newRaw = new Uint8Array(this.src.raw.length / 4);
-		for (let i = 0; i < newRaw.length; i++) {
-			newRaw[i] = this._indexedPalette.get(PNGFormatter.c2n(this.src.raw.subarray(i * 4, i * 4 + 3))) ?? 0;
-		}
-		return [palette, newRaw];
-	}
-	/**
-	 * Check if the image can be represented as grayscale with alpha.
-	 */
-	isGrayScaleAlpha(): boolean {
-		if (this.isGrayScale()) {
-			let consistentAlpha = true;
-			for (let i = 3; i < this.src.raw.length && consistentAlpha; i += 4) {
-				consistentAlpha = this.src.raw[i] === 255;
-			}
-			return !consistentAlpha;
-		}
-		return false;
-	}
-	toGrayScaleAlpha(): Uint8Array {
-		const newRaw = new Uint8Array(this.src.raw.length / 2);
-		for (let i = 0; i < newRaw.length; i += 2) {
-			newRaw[i] = this.src.raw[i * 4];
-			newRaw[i + 1] = this.src.raw[i * 4 + 3];
-		}
-		return newRaw;
-	}
-	/**
-	 * Check if an image is valid RGBA, this should be called before other validators.
-	 * @param png The png to validate, this method requires width and height.
-	 */
-	isRGBA(): boolean {
-		return this.src.width * this.src.height * 4 === this.src.raw.length;
-	}
-}
+import { PNGFormatterFrom, PNGFormatterTo } from "./format.ts";
 
 export class PNG {
 	/**
@@ -195,43 +63,15 @@ export class PNG {
 			out.raw = newRaw;
 		}
 
-		// Reformat
-		switch (dec.colorFormat) {
-			case "GrayScale": {
-				const newRaw = new Uint8Array(out.raw.length * 4);
-				for (let i = 0; i < newRaw.length; i++) {
-					newRaw[i] = (i + 1) % 4 ? out.raw[Math.floor(i / 4)] : 255;
-				}
-				out.raw = newRaw;
-				break;
+		const formatter = new PNGFormatterFrom(dec);
+		if (formatter.isCorrectFormat()) {
+			if (dec.colorFormat !== "RGBA") {
+				out.raw = formatter[`from${dec.colorFormat}`]();
 			}
-			case "RGB": {
-				const newRaw = new Uint8Array((out.raw.length * 4) / 3);
-				for (let i = 0, oldI = 0; i < newRaw.length; i++) {
-					newRaw[i] = (i + 1) % 4 ? out.raw[oldI++] : 255;
-				}
-				out.raw = newRaw;
-				break;
-			}
-			case "Indexed": {
-				const newRaw = new Uint8Array(out.raw.length * 4);
-				for (let i = 0; i < newRaw.length; i++) {
-					newRaw[i] = (i + 1) % 4 ? dec.palette[out.raw[Math.floor(i / 4)] * 3 + (i % 4)] : 255;
-				}
-				out.raw = newRaw;
-				break;
-			}
-			case "GrayScaleAlpha": {
-				const newRaw = new Uint8Array(out.raw.length * 2);
-				for (let i = 0; i < newRaw.length; i++) {
-					newRaw[i] = i % 4 === 2 ? out.raw[Math.floor(i / 2) - 1] : out.raw[Math.floor(i / 2)];
-				}
-				out.raw = newRaw;
-				break;
-			}
-			default:
-				break;
+		} else {
+			throw new Error("Image format is incorrect, check that the file isn't corrupted...");
 		}
+
 		return out;
 	}
 	/**
@@ -324,29 +164,13 @@ export class PNG {
 	 */
 	async write(path: string = "im", colorFormat?: ColorFormat, grayScaleBitDepth: BitDepth = 8): Promise<PNG> {
 		path = /.*\.png$/.test(path) ? path : path + ".png";
-		const formatter = new PNGFormatter(this);
+		const formatter = new PNGFormatterTo(this);
 		// Quick validation
-		if (!formatter.isRGBA()) throw new Error(`Image dimensions do not match pixel array length, expected ${this.width * this.height * 4}, got ${this.raw.length}...`);
+		if (!formatter.canBeRGBA()) throw new Error(`Image dimensions do not match pixel array length, expected ${this.width * this.height * 4}, got ${this.raw.length}...`);
 
 		// If the user has a specified format.
-		switch (colorFormat) {
-			case "GrayScale":
-				colorFormat = formatter.isGrayScale() ? colorFormat : undefined;
-				break;
-			case "RGB":
-				colorFormat = formatter.isRGB() ? colorFormat : undefined;
-				break;
-			case "Indexed":
-				colorFormat = formatter.isIndexed() ? colorFormat : undefined;
-				break;
-			case "GrayScaleAlpha":
-				colorFormat = formatter.isGrayScaleAlpha() ? colorFormat : undefined;
-				break;
-			case "RGBA":
-				colorFormat = formatter.isRGBA() ? colorFormat : undefined;
-				break;
-			default:
-				break;
+		if (colorFormat) {
+			colorFormat = formatter[`canBe${colorFormat}`]() ? colorFormat : undefined;
 		}
 
 		// Auto optimise format fallback
@@ -354,13 +178,13 @@ export class PNG {
 		let outRaw: Uint8Array = new Uint8Array();
 		if (!colorFormat) {
 			// First we see if it is indexed
-			if (formatter.isIndexed()) {
+			if (formatter.canBeIndexed()) {
 				[plte, outRaw] = formatter.toIndexed();
 				colorFormat = "Indexed";
 			}
 			// Now check if it can be grayscale, and if that would be more efficient.
 			// This doesn't consider the size of the plte.
-			if (formatter.isGrayScale()) {
+			if (formatter.canBeGrayScale()) {
 				if (plte.length) {
 					const indexedBits = this.paletteBitDepth(plte.length);
 					if (indexedBits >= grayScaleBitDepth) {
@@ -369,9 +193,9 @@ export class PNG {
 				} else {
 					colorFormat = "GrayScale";
 				}
-			} else if (formatter.isGrayScaleAlpha() && !plte.length) {
+			} else if (formatter.canBeGrayScaleAlpha() && !plte.length) {
 				colorFormat = "GrayScaleAlpha";
-			} else if (formatter.isRGB() && !plte.length) {
+			} else if (formatter.canBeRGB() && !plte.length) {
 				colorFormat = "RGB";
 			} else if (!plte.length) {
 				colorFormat = "RGBA";
@@ -383,29 +207,14 @@ export class PNG {
 		if (colorFormat == "Indexed" && plte.length) {
 			bitDepth = this.paletteBitDepth(plte.length);
 		} else {
-			switch (colorFormat) {
-				case "GrayScale": {
-					bitDepth = grayScaleBitDepth;
-					outRaw = formatter.toGrayScale();
-					break;
-				}
-				case "GrayScaleAlpha": {
-					outRaw = formatter.toGrayScaleAlpha();
-					bitDepth = 8;
-					break;
-				}
-				case "Indexed": {
-					[plte, outRaw] = formatter.toIndexed();
-					bitDepth = this.paletteBitDepth(plte.length);
-					break;
-				}
-				case "RGB": {
-					outRaw = formatter.toRGB();
-					bitDepth = 8;
-					break;
-				}
-				default:
-					break;
+			if (colorFormat == "Indexed") {
+				[plte, outRaw] = formatter.toIndexed();
+				bitDepth = this.paletteBitDepth(plte.length);
+			} else if (colorFormat == "GrayScale") {
+				bitDepth = grayScaleBitDepth;
+				outRaw = formatter.toGrayScale();
+			} else {
+				outRaw = formatter[`to${colorFormat!}`]();
 			}
 		}
 
